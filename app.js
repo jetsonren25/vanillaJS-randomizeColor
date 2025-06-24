63306c344c94b34fc6fdeefaf52fc535ce47a3f2
Javone <javosmith@indigoempirellc.com>
2025-06-24 09:52:54 -0400

    init commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const body = document.querySelector('body')
+const hexH1 = document.createElement('h1')
+hexH1.textContent = 'Placeholder'
+const btn = document.createElement('button')
+btn.textContent = 'Randomize'
+
+
+
+
+
+
+let alphanum = 'abcdef0123456789'
+
+
+
+
+body.appendChild(hexH1)
+body.appendChild(btn)
+
+
+
+
+
+// 1. Create a webpage that shows a random color and the HEX for it every time the btn is clicked
+// 2. You will need a btn on the screen and a font to show the HEX color on the screen
+// 3. Create a function that will randomnly select 6 characters to create a HEX color configuration
